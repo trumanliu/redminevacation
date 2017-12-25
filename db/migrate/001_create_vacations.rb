65bf8c51f47c2f157d6e60611d523a35b413f579
trumanliu <yangyangouc@gmail.com>
2017-12-26 07:22:46 +0800

    添加 vacation model

---
+++ db/migrate/001_create_vacations.rb
@@ -0,0 +1,15 @@
+class CreateVacations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :vacations do |t|
+      t.datetime :start
+      t.datetime :end
+      t.text :reason
+      t.integer :auditId
+      t.string :auditor
+      t.integer :applyId
+      t.string :applier
+      t.integer :status
+      t.string :statusShow
+    end
+  end
+end
--- init.rb
+++ init.rb
@@ -6,6 +6,8 @@ Redmine::Plugin.register :ingeekvacation do
   url 'https://github.com/trumanliu/ingeekvacation'
   author_url 'trumanliu.com'
 
+  permission :休假管理, :vacation => :index
   menu :top_menu, :vacation, { :controller => 'vacation', :action => 
   'index' }, :caption => '休假申请' 
+
 end

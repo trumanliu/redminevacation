d12dcd632a7cdfcd82c4e3f022dfdbeb146f502f
trumanliu <yangyangouc@gmail.com>
2018-01-02 23:06:25 +0800

    update sql
    更新 SQL 文件

--- db/iss.sql
+++ db/iss.sql
@@ -1,9 +1,6 @@
-ALTER TABLE `redmine`.`time_entries` 
+ALTER TABLE `redmine3`.`time_entries` 
 ADD COLUMN `audit_id` INT(11) NULL AFTER `updated_on`,
 ADD COLUMN `audit_name` VARCHAR(45) NULL AFTER `audit_id`,
-ADD COLUMN `time_type` VARCHAR(45) NULL AFTER `audit_name`,
-ADD COLUMN `time_typename` VARCHAR(45) NULL AFTER `time_type`,
-ADD COLUMN `audit_hours` VARCHAR(45) NULL AFTER `time_typename`;
-
-ALTER TABLE `redmine`.`time_entries` 
-ADD COLUMN `audit_status` VARCHAR(45) NULL AFTER `audit_hours`;
+ADD COLUMN `audit_hours` VARCHAR(45) NULL AFTER `audit_name`,
+ADD COLUMN `audit_status` VARCHAR(45) NULL AFTER `audit_hours`,
+ADD COLUMN `username` VARCHAR(45) NULL AFTER `audit_status`;
